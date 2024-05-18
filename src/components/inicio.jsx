63a5bbea2bead5711dce30a531c837c22b1aa5da
Uzai Baltazar <uzai.baltazar23b@udgvirtual.udg.mx>
2024-05-18 16:16:26 -0600

    Ajuste de nombre en Inicio

--- src/components/inicio.jsx
+++ src/components/inicio.jsx
@@ -3,8 +3,13 @@ import udg from '../assets/img/udg.png'
 const Inicio = () => {
     return(
         <div className="letreroInicio">
-            <img className='logo' src={udg} alt="Logo UdG" />            
+            <img className='logo' src={udg} alt="Logo UdG" />
+            
+            <p className='udg'>UdG|Virtual</p>
+            <span className='nombre'>Uzai Baltazar S.</span>
+            
         </div>
+
     )
 }
 export default Inicio
