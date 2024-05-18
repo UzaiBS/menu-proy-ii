1df867adf62f6f22cee922f2105b7613792b0774
Uzai Baltazar <uzai.baltazar23b@udgvirtual.udg.mx>
2024-05-18 15:52:14 -0600

    Arreglo CSS

--- src/components/inicio.jsx
+++ src/components/inicio.jsx
@@ -3,8 +3,7 @@ import udg from '../assets/img/udg.png'
 const Inicio = () => {
     return(
         <div className="letreroInicio">
-            <img className='logo' src={udg} alt="Logo UdG" />
-            <h1>¡Bienvenidos!</h1>
+            <img className='logo' src={udg} alt="Logo UdG" />            
         </div>
     )
 }
